5d6e549b31f76e6e737b5d1ec7dd93773fe3b2c6
Mauricio Erazo <antonierazo13@hotmail.com>
2022-11-11 16:45:31 +0100

    fix: now i can get the data
    i close and open VS then it could work

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 // This is goint to be root, the request are gonna be made with the root + username and repos
 const APIURL = 'https://api.github.com/users/'
 
-getUser('bradtraversy')
+getUser('wichofly')
 
-async function getUser(username) {
-	// we can destructure to get just the data "{data}"
-	const { data } = await axios(APIURL + username)
+// async function getUser(username) {
+// 	// we can destructure to get just the data "{data}"
+// 	const { data } = await axios(APIURL + username)
 
-	console.log(data)
-}
+// 	console.log(data)
+// }
 
 // Example axios without async await
 // ----------------------------
-// function getUser(username) {
-// 	// As well can be written as axios.get(...)
-// 	axios(APIURL + username)
-// 		// ** One advantage using axios is that we dont have to write "res.json()"
-// 		// to then make another promise and get the data e.g. ".then((data) => {} **"
-// 		.then(res => console.log(res.data))
-// 		.catch(err => console.log(err))
-// }
+function getUser(username) {
+  // As well can be written as axios.get(...)
+  axios(APIURL + username)
+    // ** One advantage using axios is that we dont have to write "res.json()"
+    // to then make another promise and get the data e.g. ".then((data) => {} **"
+    .then(res => console.log(res.data))
+    .catch(err => console.log(err))
+}
 
