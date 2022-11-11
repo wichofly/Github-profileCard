278068893b2f80323b147178e77fbcf45bf5a953
Mauricio Erazo <antonierazo13@hotmail.com>
2022-11-11 16:49:50 +0100

    feat: try catch to handle errors

--- script.js
+++ script.js
@@ -3,21 +3,26 @@ const APIURL = 'https://api.github.com/users/'
 
 getUser('wichofly')
 
-// async function getUser(username) {
-// 	// we can destructure to get just the data "{data}"
-// 	const { data } = await axios(APIURL + username)
+async function getUser(username) {
+  try {
+    // we can destructure to get just the data "{data}"
+    const { data } = await axios(APIURL + username)
 
-// 	console.log(data)
-// }
+    console.log(data)
+  } catch (err) {
+    console.log(err)
+  }
+
+}
 
 // Example axios without async await
 // ----------------------------
-function getUser(username) {
-  // As well can be written as axios.get(...)
-  axios(APIURL + username)
-    // ** One advantage using axios is that we dont have to write "res.json()"
-    // to then make another promise and get the data e.g. ".then((data) => {} **"
-    .then(res => console.log(res.data))
-    .catch(err => console.log(err))
-}
+// function getUser(username) {
+//   // As well can be written as axios.get(...)
+//   axios(APIURL + username)
+//     // ** One advantage using axios is that we dont have to write "res.json()"
+//     // to then make another promise and get the data e.g. ".then((data) => {} **"
+//     .then(res => console.log(res.data))
+//     .catch(err => console.log(err))
+// }
 
